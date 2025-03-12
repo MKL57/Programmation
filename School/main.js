76a13398928ef9f598dd320c9316012d4b92383c
MKL57 <jackmondza@gmail.com>
2025-03-12 04:07:42 +0100

    Add files via upload

---
+++ School/main.js
@@ -0,0 +1,35 @@
+var typed= new Typed(".text", {
+    strings:["Etudiant en 2e année à l'école universitaire 'HEMIP'"],
+    typeSpeed:30,
+    backSpeed:10,
+    backDelay:2000,
+    loop:true,
+});
+var typed= new Typed(".note", {
+    strings:["Génie Logiciel"],
+    typeSpeed:100,
+    backSpeed:10,
+    backDelay:2000,
+    loop:true,
+});
+var typed= new Typed(".project", {
+    strings:["Création d'un logiciel d'application" , "Conception d'un réseau sécurisé"],
+    typeSpeed:40,
+    backSpeed:10,
+    backDelay:2000,
+    loop:true,
+});
+var typed= new Typed(".exp", {
+    strings:["Baccalauréat de l'année 2023"],
+    typeSpeed:40,
+    backSpeed:10,
+    backDelay:2000,
+    loop:true,
+});
+var typed= new Typed(".skill", {
+    strings:["Office : Word, Excel, PowerPoint" , "Algorithme : Codeblocks, Algobox, Eclipse" , "Langue : Français, Anglais, Espagnol (intermédiaire)"],
+    typeSpeed:40,
+    backSpeed:10,
+    backDelay:2000,
+    loop:true,
+});
